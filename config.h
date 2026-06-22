c11849fd7f05e31c0e4da29d549da811ea396e46
takyz <test@test.com>
2026-06-21 21:41:57 -0300

    Hola mundo

--- config.h
+++ config.h
@@ -203,8 +203,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ 0,                    0,              kscrollup,      {.i = -1} },
-    { 0,                    0,              kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_k,           kscrollup,      {.i = -1} },
+    { MODKEY,               XK_j,           kscrolldown,    {.i = -1} },
 };
 
 /*
